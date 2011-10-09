d732206a09fd3cc26f53fec5e7954a2b3480d5f4
Cynbe <cynbe@mythryl.org>
2011-10-08 22:28:18 -0700

    2011-10-08-22.28.02: src/c/lib/posix-os/tmpname.c: Rewrote to not use tmpnam(), which might be a bit better and suppresses a warning.  Created src/c/lib/posix-file-system/mkstemp.c, recommended as a more secure alternative, accessible via src/lib/std/src/posix-1003.1b/posix-file.pkg: UNTESTED.

---
+++ src/c/lib/posix-file-system/mkstemp.c
@@ -0,0 +1,64 @@
+// openf.c
+
+
+#include "../../config.h"
+
+#include <stdio.h>
+#include <errno.h>
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include "system-dependent-unix-stuff.h"
+
+#if HAVE_FCNTL_H
+    #include <fcntl.h>
+#endif
+
+#include "make-strings-and-vectors-etc.h"
+    #include "lib7-c.h"
+#include "cfun-proto-list.h"
+
+
+
+// One of the library bindings exported via
+//     src/c/lib/posix-file-system/cfun-list.h
+// and thence
+//     src/c/lib/posix-file-system/libmythryl-posix-file-system.c
+
+
+
+Val   _lib7_P_FileSys_mkstemp   (Task* task,  Val arg)   {
+    //=======================
+    //
+    // Mythryl type:  Void -> Int
+    //
+    // Open a temporary file and return the file descriptor.
+    //
+    // This fn gets bound as   tmpfile'   in:
+    //
+    //     src/lib/std/src/posix-1003.1b/posix-file.pkg
+    //     src/lib/std/src/posix-1003.1b/posix-file-system-64.pkg
+    char buf[ 32 ];
+
+    int	 fd;
+
+    strcpy( buf, "tmpfile.XXXXXX" );		// Must end with exactly six 'X's -- see man mkstemp(3).
+
+/*  do { */					// Backed out 2010-02-26 CrT: See discussion at bottom of src/c/lib/socket/connect.c
+
+    fd  =  mkstemp( buf );
+
+/*  } while (fd < 0 && errno == EINTR);	*/	// Restart if interrupted by a SIGALRM or SIGCHLD or whatever. HAVEN"T CHECKED WHETHER mkstemp IS INTERRUPTABLE -- this is copied blindly from openf.c.
+
+
+    CHECK_RETURN_VAL(task, fd, TAGGED_INT_FROM_C_INT( fd ));
+}
+
+
+
+// COPYRIGHT (c) 1995 by AT&T Bell Laboratories.
+// Subsequent changes by Jeff Prothero Copyright (c) 2010-2011,
+// released under Gnu Public Licence version 3.
+
--- src/c/lib/posix-os/tmpname.c
+++ src/c/lib/posix-os/tmpname.c
@@ -4,19 +4,24 @@
 #include "../../config.h"
 
 #include <stdio.h>
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+
 #include "runtime-base.h"
 #include "runtime-values.h"
 #include "make-strings-and-vectors-etc.h"
 #include "cfun-proto-list.h"
-
-
-
-// XXX BUGGO FIXME:
-// tmpname.c:(.text+0xe): warning: the use of `tmpnam' is dangerous, better use `mkstemp'
-// -- but the mkstemp() manpage says:
-//
-//      "Don't use this function, use tmpfile(3) instead.
-//       It is better  defined and more portable."
 
 
 
@@ -30,21 +35,54 @@
 Val   _lib7_OS_tmpname   (Task* task,  Val arg)   {
     //================
     //
-    // Mythryl type:   Void -> String
+    // Generate a unique name for a temporary file.
     //
+    // Mythryl type:   Void -> String
     //
     // This fn gets bound as   tmp_name   in:
     //
     //     src/lib/std/src/posix/winix-file.pkg
 
+    static int call_number = 0;
 
-    char buf[ L_tmpnam ];
-    //
-    tmpnam( buf );
+    char buf[ 132 ];
+    
+    int c1 = ++call_number;		// Try to make our filename unique.
+    
+    int pid = getpid();			// Try to harder to make our filename unique.
+
+    int c2 = ++call_number;		// Try to harder yet to make our filename unique. :-)
+ 
+    sprintf (buf, "tmpfile.%d.%d.%d.tmp", c1, pid, c2);
     //
     return make_ascii_string_from_c_string (task, buf);
 }
-
+//
+// NOTES:
+//     Here we used to use tmpnam(), but that generated warnings that it is dangerous,
+//     and that it is better to use mkstemp()  -- but the mkstemp() manpage says:
+//
+//        "Don't use this function, use tmpfile(3) instead.
+//         It is better  defined and more portable."
+//    
+//     They seem to mainly be worried about malicious processes guessing the tmpfile
+//     name and cracking security by overwriting the file.  (Personally, I'm not too
+//     worried about this, I figure that if malicious agents control a process on
+//     my machine, I'm already toast -- and anyhow we're no longer living in the old
+//     world of campus timesharing Unix machines!)
+//
+//     tmpfile() doesn't fit Mythryl very well because it returns a FILE*,
+//     and as a rule we don't use C buffered I/O in Mythryl.
+//
+//     mkstemp() returns an fd, which fits Mythryl better,
+//     so we now provide it via
+//
+//         src/c/lib/posix-file-system/mkstemp.c
+//
+//     But it is no use if we're generating a bash commandline line to execute,
+//     so we continue to provide this function.
+//
+//         -- 2011-10-08 CrT
 
 // COPYRIGHT (c) 1994 by AT&T Bell Laboratories.
 // Subsequent changes by Jeff Prothero Copyright (c) 2010-2011,
