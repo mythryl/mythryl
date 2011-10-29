e94fa655aba28b45e76cf1b6eae753b363fbe442
Cynbe <cynbe@mythryl.org>
2011-10-28 19:04:13 -0700

    2011-10-28-19.03.56: src/c/lib/pthread/:  Combined the pre-existing files libmythryl-pthread.c, cfun-list.h, cfun-proto-list.h, max-pthreads.c, release-pthread.c and spin-lock.c into a single file libmythryl-pthread.c.  This is less mickeymouse, more maintaianble, and declutters the global namespace by making all the functions static (local) instead of global.

--- src/c/lib/pthread/libmythryl-pthread.c
+++ src/c/lib/pthread/libmythryl-pthread.c
@@ -1,5 +1,5 @@
 // libmythryl-pthread.c
-//
+
 // Here we export to
 //
 //     src/lib/std/src/pthread.api
@@ -14,14 +14,21 @@
 //     src/c/pthread/pthread-on-posix-threads.c
 //     src/c/pthread/pthread-on-sgi.c
 //     src/c/pthread/pthread-on-solaris.c
+//
+// For background see:
+//
+//     src/A.MULTICORE-SUPPORT.OVERVIEW
 
 
 #include "../../config.h"
 
 #include "runtime-base.h"
-#include "mythryl-callable-c-libraries.h"
-#include "cfun-proto-list.h"
+#include "runtime-pthread.h"
+#include "runtime-values.h"
+#include "runtime-globals.h"
 
+#include "make-strings-and-vectors-etc.h"
+#include "mythryl-callable-c-libraries.h"
 
 // This file defines the "pthread" library of Mythryl-callable
 // C functions, accessible at the Mythryl level via:
@@ -32,28 +39,98 @@
 // 
 // or such.
 
-#define CFUNC(NAME, NAME2, FUNC, LIB7TYPE)	CFUNC_BIND(NAME, NAME2, FUNC, LIB7TYPE)
+
+static Val   acquire_pthread   (Task* task,  Val arg)   {			// Apparently never called.
+    //       ===============
+    //
+    #ifdef MULTICORE_SUPPORT
+	//
+	return pth_acquire_pthread( task, arg );				// pth_acquire_pthread	def in    src/c/pthread/pthread-on-posix-threads.c
+        //									// pth_acquire_pthread	def in    src/c/pthread/pthread-on-sgi.c
+    #else									// pth_acquire_pthread	def in    src/c/pthread/pthread-on-solaris.c
+	die ("lib7_acquire_pthread: no mp support\n");
+        return HEAP_TRUE;							// Cannot execute; only present to quiet gcc.
+    #endif
+}
+
+
+
+static Val release_pthread   (Task* task,  Val arg)   {
+    //     ===============
+    //
+    #ifdef MULTICORE_SUPPORT
+	pth_release_pthread(task);  	// Should not return.
+	die ("_lib7_MP_release_pthread: call unexpectedly returned\n");
+    #else
+	die ("_lib7_MP_release_pthread: no mp support\n");
+        return HEAP_VOID;							// Cannot execute; only present to quiet gcc.
+    #endif
+}
+
+
+static Val   max_pthreads   (Task* task,  Val arg)   {				// Apparently nowhere invoked.
+    //       ============
+    //
+    #ifdef MULTICORE_SUPPORT
+	return TAGGED_INT_FROM_C_INT(pth_max_pthreads ());
+    #else
+	die ("_lib7_MP_max_pthreads: no mp support\n");
+        return TAGGED_INT_FROM_C_INT( 0 );					// Cannot execute; only present to quiet gcc.
+    #endif
+}
+
+
+static Val   spin_lock   (Task* task,  Val arg)   {
+    //       =========
+    //
+    #ifdef MULTICORE_SUPPORT
+	// "This code is for use the assembly (MIPS.prim.asm) try_lock and lock"
+        //         --- Original SML/NJ comment.
+        //
+        // 2010-11-30 CrT:
+        //     'try_lock' is defined in
+        //                     src/c/machine-dependent/prim.intel32.asm
+	//                     src/c/machine-dependent/prim.sparc32.asm
+        //                     src/c/machine-dependent/prim.pwrpc32.asm
+        //                     src/c/machine-dependent/prim.intel32.masm 
+        //     but function makes no obvious use of them.
+        //     'try_lock is also published in RunVec in
+        //                     src/c/main/construct-runtime-package.c
+        //     -- possibly that route replaced this one, which bitrotted?
+	Val             result;
+	REF_ALLOC(task, result, HEAP_FALSE);						// REF_ALLOC	def in    src/c/h/make-strings-and-vectors-etc.h
+	return          result;
+    #else
+	die ("lib7_spin_lock: no mp support\n");
+        return HEAP_VOID;							// Cannot execute; only present to quiet gcc.
+    #endif
+}
+
 static Mythryl_Name_With_C_Function CFunTable[] = {
-#include "cfun-list.h"											// Actual function list is in src/c/lib/pthread/cfun-list.h
-	CFUNC_NULL_BIND
-    };
-#undef CFUNC
+    //
+    { "acquire_pthread","acquire_pthread",	acquire_pthread,	""},
+    { "max_pthreads","max_pthreads",		max_pthreads,		""},
+    { "release_pthread","release_pthread",	release_pthread,	""},
+    { "spin_lock","spin_lock",			spin_lock,		""},
+    //
+    CFUNC_NULL_BIND
+};
 
 
-// The posix-thread ("pthread") library.
+// The pthread (multicore support) library:
 //
 // Our record                Libmythryl_Pthread
 // gets compiled into        src/c/lib/mythryl-callable-c-libraries-list.h
 // and thus ultimately       mythryl_callable_c_libraries_local []
-// in                        src/c/lib/c-libraries.c
+// in                        src/c/lib/mythryl-callable-c-libraries.c
 //
 Mythryl_Callable_C_Library	    Libmythryl_Pthread = {						// Mythryl_Callable_C_Library		def in    src/c/h/mythryl-callable-c-libraries.h
-    //                              ====================
-    CLIB_NAME,
-    CLIB_VERSION,
-    CLIB_DATE,
+    //                              ==================
+    "pthread",			// Library name.
+    "1.0",			// Library version.
+    "December 18, 1994",	// Library creation date.
     NULL,
-    CFunTable
+    CFunTable			// Library functions.
 };
 
 
