4c2d0c4a6ad760ea3b51766e2bf51bf5ba20be18
Cynbe <cynbe@mythryl.org>
2011-11-20 14:19:44 -0800

    2011-11-20-14.19.26: src/c/lib/posix-file-system/mkfifo.c:  Added buffer/unbuffer RELEASE/RECOVER calls.

--- src/c/lib/posix-file-system/mkfifo.c
+++ src/c/lib/posix-file-system/mkfifo.c
@@ -46,8 +46,22 @@ Val   _lib7_P_FileSys_mkfifo   (Task* task,  Val arg)   {
     Val	    path = GET_TUPLE_SLOT_AS_VAL(    arg, 0);
     mode_t  mode = TUPLE_GETWORD(arg, 1);
     //
-    int status = mkfifo (HEAP_STRING_AS_C_STRING(path), mode);
+    char*  heap_path = HEAP_STRING_AS_C_STRING( path );
     //
+    Mythryl_Heap_Value_Buffer  path_buf;
+    //
+    char* c_path
+	= 
+        buffer_mythryl_heap_value( &path_buf, (void*) heap_path, strlen( heap_path ) +1 );		// '+1' for terminal NUL on string.
+
+    RELEASE_MYTHRYL_HEAP( task->pthread, "_lib7_P_FileSys_mkfifo", arg );
+	//
+        int status = mkfifo (c_path, mode);
+	//
+    RECOVER_MYTHRYL_HEAP( task->pthread, "_lib7_P_FileSys_mkfifo" );
+
+    unbuffer_mythryl_heap_value( &path_buf );
+
     CHECK_RETURN_UNIT(task, status)
 }
 
