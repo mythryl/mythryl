ea5ce555f720311e96add636a08c3d1ae9d88050
Cynbe <cynbe@mythryl.org>
2011-11-21 03:11:33 -0800

    2011-11-21-03.11.16: src/c/lib/posix-io/writebuf.c:  Added buffer/unbuffer RELEASE/RECOVER calls.

--- src/c/lib/posix-io/writebuf.c
+++ src/c/lib/posix-io/writebuf.c
@@ -3,6 +3,8 @@
 
 #include "../../mythryl-config.h"
 
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
 
 #include "system-dependent-unix-stuff.h"
@@ -39,18 +41,37 @@ Val   _lib7_P_IO_writebuf   (Task* task,  Val arg)   {
     //     src/lib/std/src/posix-1003.1b/posix-io.pkg
     //     src/lib/std/src/posix-1003.1b/posix-io-64.pkg
 
-    int		fd     = GET_TUPLE_SLOT_AS_INT(                     arg, 0);
-    Val		start  = GET_TUPLE_SLOT_AS_VAL(                        arg, 1);
-    size_t	nbytes = GET_TUPLE_SLOT_AS_INT(                     arg, 2);
-    char	*data  = HEAP_STRING_AS_C_STRING(start) + GET_TUPLE_SLOT_AS_INT(arg, 3);
+    int		fd         = GET_TUPLE_SLOT_AS_INT(                                 arg, 0);
+    Val		start      = GET_TUPLE_SLOT_AS_VAL(                                 arg, 1);
+    size_t	nbytes     = GET_TUPLE_SLOT_AS_INT(                                 arg, 2);
+    char*	heap_data  = HEAP_STRING_AS_C_STRING(start) + GET_TUPLE_SLOT_AS_INT(arg, 3);
+
 
     ssize_t    	n;
 
+    // We cannot reference anything on the Mythryl
+    // heap after we do RELEASE_MYTHRYL_HEAP
+    // because garbage collection might be moving
+    // it around, so copy heap_data into C storage: 
+    //
+    Mythryl_Heap_Value_Buffer  data_buf;
+    //
+    {	char* c_data
+	    = 
+	    buffer_mythryl_heap_value( &data_buf, (void*) heap_data, nbytes );
+
 /*  do { */					// Backed out 2010-02-26 CrT: See discussion at bottom of src/c/lib/socket/connect.c
 
-        n = write (fd, data, nbytes);
+	RELEASE_MYTHRYL_HEAP( task->pthread, "_lib7_P_IO_writebuf", arg );
+	    //
+	    n = write (fd, c_data, nbytes);
+	    //
+	RECOVER_MYTHRYL_HEAP( task->pthread, "_lib7_P_IO_writebuf" );
 
 /*  } while (n < 0 && errno == EINTR);	*/	// Restart if interrupted by a SIGALRM or SIGCHLD or whatever.
+
+	unbuffer_mythryl_heap_value( &data_buf );
+    }
 
     CHECK_RETURN (task, n)
 }
