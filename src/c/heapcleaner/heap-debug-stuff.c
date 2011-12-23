1c221512c58e4391a69efa9d775cba2b247448bd
Cynbe <cynbe@mythryl.org>
2011-12-23 13:49:10 -0800

    2011-12-23-13.48.54: src/c/heapcleaner/heap-debug-stuff.c: Rewrote to write dumps to separate files, to improve readability of main logfile and facilitating before-and-after grepping of dumps.

--- src/c/heapcleaner/heap-debug-stuff.c
+++ src/c/heapcleaner/heap-debug-stuff.c
@@ -4,8 +4,11 @@
 
 #include "../mythryl-config.h"
 
+#include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <errno.h>
+#include <pthread.h>
 
 #if HAVE_SYS_TIME_H
 #  include <sys/time.h>
@@ -86,88 +89,150 @@ void   check_agegroup0_overrun_tripwire_buffer( Task* task, char* caller ) {
 #endif
 }
 
+static FILE*   open_heapdump_logfile   (char* filename_buf, size_t bufsize, char* dumptype) {
+    //         =====================
+    //
+    // The idea here is to write each heap snapshot to a separate file
+    // to facilitate diff-ing between them and to avoid spamming the
+    // main logfile for the process.
+    //
+    // We want to minimize the chance of two dumps clobbering
+    // each other, so we make the snapshot filename depend on:
+    //
+    //     getpid()
+    //     pthread_self()
+    //     gettimeofday()
+    //     sequentially assigned dump number.
+    //
+    struct timeval tv;
+    //
+    if (gettimeofday(&tv,NULL)) {
+	fprintf(stderr,"open_heapdump_logfile: gettimeofday failed: %s\n", strerror(errno) );
+	exit(1);
+    }
+
+    static int dump_number = 0;
+
+    int pid    = getpid();
+    int	c_sec  = tv.tv_sec;									// Current time to second accuracy.
+    int	c_usec = tv.tv_usec;									// Fractional current time to nominal microsecond accuracy, usually actually good to roughly millisecond resolution.
+    int ptid   = (int)(pthread_self());
+    //
+    snprintf(filename_buf, bufsize, "%sdump-%08x-%08x-%08d.%06d-%d.log", dumptype, pid, ptid, c_sec, c_usec, dump_number++);
+
+    FILE* fd = fopen(filename_buf, "w");
+    if (!fd) {
+	fprintf(stderr,"open_heapdump_logfile: Unable to fopen(%s): %s\n", filename_buf, strerror(errno) );
+	exit(1);
+    }
+    return fd;
+}
+
+static void   close_heapdump_logfile   (FILE* fd, char* filename) {
+    //        ======================
+    //
+    if (fclose(fd)) {
+	fprintf(stderr,"close_heapdump_logfile: Unable to fclose(%s): %s\n", filename, strerror(errno) );
+	exit(1);
+    }
+}
+
+static unsigned int   v2u   (Val v) { return (unsigned int) v; }		// More type-safe than using a cast:  Ensures that we really are converting a Val.
+//                    ===
+
 // Write to logfile contents of a Task record:
 //
-void log_task( Task* task, char* caller ) {
+void   log_task   (Task* task, char* caller) {
+    // ========
     //
-    log_if("log_task:                     caller s=%s",  caller);
-    log_if("log_task:                       task x=%p",  task);
-    log_if("log_task:                       heap x=%x",  task->heap);
-    log_if("log_task:                    pthread x=%x",  task->pthread);
-    log_if("log_task:                pthread->id x=%x",  (unsigned int)(task->pthread->tid));
-    log_if("log_task:    heap_allocation_pointer x=%x",  task->heap_allocation_pointer);
-    log_if("log_task:      heap_allocation_limit x=%x",  task->heap_allocation_limit);
-    log_if("log_task: real_heap_allocation_limit x=%x",  task->real_heap_allocation_limit);
-    log_if("log_task:                   argument x=%x",  task->argument);
-    log_if("log_task:                       fate x=%x",  task->fate);
-    log_if("log_task:            current_closure x=%x",  task->current_closure);
-    log_if("log_task:              link_register x=%x",  task->link_register);
-    log_if("log_task:            program_counter x=%x",  task->program_counter);
-    log_if("log_task:             exception_fate x=%x",  task->exception_fate);
-    log_if("log_task:             current_thread x=%x",  task->current_thread);
-    log_if("log_task:             heap_changelog x=%x",  task->heap_changelog);
-    log_if("log_task:            fault_exception x=%x",  task->fault_exception);
-    log_if("log_task:   faulting_program_counter x=%x",  task->faulting_program_counter);
-    log_if("log_task:            protected_c_arg x=%x",  task->protected_c_arg);
-    log_if("log_task:                  &heapvoid x=%x", &task->heapvoid);
-    log_if("log_task: Following stuff is in task->heap:");
-    log_if("log_task:           agegroup0_buffer x=%x",  task->heap->agegroup0_buffer);
-    log_if("log_task:  agegroup0_buffer_bytesize x=%x",  task->heap->agegroup0_buffer_bytesize);
-    log_if("log_task:           sum of above two x=%x",  (char*)(task->heap->agegroup0_buffer) + task->heap->agegroup0_buffer_bytesize);
-    log_if("log_task:       multipage_ram_region x=%x",  task->heap->multipage_ram_region);
-    log_if("log_task:           active_agegroups d=%d",  task->heap->active_agegroups);
-    log_if("log_task: oldest_agegroup_keeping_idle_fromspace_buffers d=%d",  task->heap->oldest_agegroup_keeping_idle_fromspace_buffers);
-    log_if("log_task:  hugechunk_ramregion_count d=%d",  task->heap->hugechunk_ramregion_count);
-    log_if("log_task:      total_bytes_allocated x=(%x,%x) (millions, 1s)",  task->heap->total_bytes_allocated.millions, task->heap->total_bytes_allocated.ones );
+    char filename[ 1024 ];
+    FILE* fd = open_heapdump_logfile( filename, 1024, "task" );
+    //
+    log_if("log_task: Starting dump to '%s'", filename);
+
+    fprintf(fd,"                     caller s=%s\n",  caller);
+    fprintf(fd,"                       task p=%p\n",  task);
+    fprintf(fd,"                       heap p=%p\n",  task->heap);
+    fprintf(fd,"                    pthread p=%p\n",  task->pthread);
+    fprintf(fd,"                pthread->id x=%x\n",  (unsigned int)(task->pthread->tid));
+    fprintf(fd,"    heap_allocation_pointer p=%p\n",  task->heap_allocation_pointer);
+    fprintf(fd,"      heap_allocation_limit p=%p\n",  task->heap_allocation_limit);
+    fprintf(fd," real_heap_allocation_limit p=%p\n",  task->real_heap_allocation_limit);
+    fprintf(fd,"                   argument x=%x\n",  v2u( task->argument ));
+    fprintf(fd,"                       fate x=%x\n",  v2u( task->fate ));
+    fprintf(fd,"            current_closure x=%x\n",  v2u( task->current_closure ));
+    fprintf(fd,"              link_register px=%x\n", v2u( task->link_register ));
+    fprintf(fd,"            program_counter x=%x\n",  v2u( task->program_counter ));
+    fprintf(fd,"             exception_fate x=%x\n",  v2u( task->exception_fate ));
+    fprintf(fd,"             current_thread x=%x\n",  v2u( task->current_thread ));
+    fprintf(fd,"             heap_changelog x=%x\n",  v2u( task->heap_changelog ));
+    fprintf(fd,"            fault_exception x=%x\n",  v2u( task->fault_exception ));
+    fprintf(fd,"   faulting_program_counter x=%x\n",  (unsigned int) task->faulting_program_counter);	// Val_Sized_Unt.
+    fprintf(fd,"            protected_c_arg p=%p\n",  task->protected_c_arg);
+    fprintf(fd,"                  &heapvoid p=%p\n", &task->heapvoid);
+    fprintf(fd," Following stuff is in task->heap:\n");
+    fprintf(fd,"           agegroup0_buffer p=%p\n",  task->heap->agegroup0_buffer);
+    fprintf(fd,"  agegroup0_buffer_bytesize x=%x\n",  (unsigned int) task->heap->agegroup0_buffer_bytesize);	// Punt
+    fprintf(fd,"           sum of above two p=%p\n",  (char*)(task->heap->agegroup0_buffer) + task->heap->agegroup0_buffer_bytesize);
+    fprintf(fd,"       multipage_ram_region p=%p\n",  task->heap->multipage_ram_region);
+    fprintf(fd,"           active_agegroups d=%d\n",  task->heap->active_agegroups);
+    fprintf(fd," oldest_agegroup_keeping_idle_fromspace_buffers d=%d\n",  task->heap->oldest_agegroup_keeping_idle_fromspace_buffers);
+    fprintf(fd,"  hugechunk_ramregion_count d=%d\n",  task->heap->hugechunk_ramregion_count);
+    fprintf(fd,"      total_bytes_allocated x=(%x,%x) (millions, 1s)\n",  (unsigned int)task->heap->total_bytes_allocated.millions, (unsigned int)task->heap->total_bytes_allocated.ones );
 
     for (int i = 0; i < task->heap->active_agegroups; ++i) {
 	//
 	Agegroup* a = task->heap->agegroup[ i ];
-	log_if("log_task:           agegroup[%d] x=%x (holds agegroup %d)",  i, a, i+1);
-	log_if("log_task:           a->age       d=%d",  a->age);
-	log_if("log_task:           a->cleanings d=%d",  a->cleanings);
-	log_if("log_task:           a->ratio     x=%x (Desired number of collections of the previous agegroup for one collection of this agegroup)",  a->ratio);
-	log_if("log_task:           a->last_cleaning_count_of_younger_agegroup d=%d",  a->last_cleaning_count_of_younger_agegroup);
+	fprintf(fd,"           agegroup[%d] p=%p (holds agegroup %d)\n",  i, a, i+1);
+	fprintf(fd,"           a->age       d=%d\n",  a->age);
+	fprintf(fd,"           a->cleanings d=%d\n",  a->cleanings);
+	fprintf(fd,"           a->ratio     x=%x (Desired number of collections of the previous agegroup for one collection of this agegroup)\n",  a->ratio);
+	fprintf(fd,"           a->last_cleaning_count_of_younger_agegroup d=%d\n",  a->last_cleaning_count_of_younger_agegroup);
 
 	for (int s = 0; s < MAX_PLAIN_ILKS; ++s) {
 	    //
-	    log_if("log_task:");
-	    log_if("log_task:          a->sib[%d]    x=%x",  s, a->sib[s]);
-	    log_if("log_task:          a->sib[%d].id d=%d",  s, a->sib[s]->id);
-	    log_if("log_task:");
-	    log_if("log_task:          a->sib[%d].tospace            x=%x",  s, a->sib[s]->tospace);
-	    log_if("log_task:          a->sib[%d].tospace_bytesize   x=%x",  s, a->sib[s]->tospace_bytesize);
-	    log_if("log_task:          a->sib[%d].tospace_limit      x=%x",  s, a->sib[s]->tospace_limit);
-	    log_if("log_task:");
-	    log_if("log_task:          a->sib[%d].fromspace          x=%x",  s, a->sib[s]->fromspace);
-	    log_if("log_task:          a->sib[%d].fromspace_bytesize x=%x",  s, a->sib[s]->fromspace_bytesize);
-	    log_if("log_task:          a->sib[%d].fromspace_used_end x=%x",  s, a->sib[s]->fromspace_used_end);
-	    log_if("log_task:");
-	    log_if("log_task:          a->sib[%d].next_tospace_word_to_allocate x=%x",  s, a->sib[s]->next_tospace_word_to_allocate);
-	    log_if("log_task:          a->sib[%d].next_word_to_sweep_in_tospace x=%x",  s, a->sib[s]->next_word_to_sweep_in_tospace);
-	    log_if("log_task:          a->sib[%d].repairlist x=%x",  s, a->sib[s]->repairlist);
-	    log_if("log_task:          a->sib[%d].end_of_fromespace_oldstuff x=%x",  s, a->sib[s]->end_of_fromspace_oldstuff);
+	    fprintf(fd,"\n");
+	    fprintf(fd,"          a->sib[%d]    p=%p\n",  s, a->sib[s]);
+	    fprintf(fd,"          a->sib[%d].id d=%d\n",  s, a->sib[s]->id);
+	    fprintf(fd,"\n");
+	    fprintf(fd,"          a->sib[%d].tospace            p=%p\n",  s, a->sib[s]->tospace);
+	    fprintf(fd,"          a->sib[%d].tospace_bytesize   p=%p\n",  s, (void*)a->sib[s]->tospace_bytesize);		// Punt
+	    fprintf(fd,"          a->sib[%d].tospace_limit      p=%p\n",  s, a->sib[s]->tospace_limit);
+	    fprintf(fd,"\n");
+	    fprintf(fd,"          a->sib[%d].fromspace          p=%p\n",  s, a->sib[s]->fromspace);
+	    fprintf(fd,"          a->sib[%d].fromspace_bytesize x=%x\n",  s, (unsigned int) a->sib[s]->fromspace_bytesize);	// Val_Sized_Unt
+	    fprintf(fd,"          a->sib[%d].fromspace_used_end p=%p\n",  s, a->sib[s]->fromspace_used_end);
+	    fprintf(fd,"\n");
+	    fprintf(fd,"          a->sib[%d].next_tospace_word_to_allocate p=%p\n",  s, a->sib[s]->next_tospace_word_to_allocate);
+	    fprintf(fd,"          a->sib[%d].next_word_to_sweep_in_tospace p=%p\n",  s, a->sib[s]->next_word_to_sweep_in_tospace);
+	    fprintf(fd,"          a->sib[%d].repairlist p=%p\n",  s, a->sib[s]->repairlist);
+	    fprintf(fd,"          a->sib[%d].end_of_fromespace_oldstuff p=%p\n",  s, a->sib[s]->end_of_fromspace_oldstuff);
 	}
     }
+    close_heapdump_logfile( fd, filename );
+    log_if("log_task: Dump to '%s' now complete.", filename);
 }
 
 // Write to logfile contents of the generation0
 // buffer for a given Task.  No attempt is made
-// to distinguish garbage from live data.
+// to distinguish live from dead data.
 //
-void log_gen0( Task* task, char* caller ) {
+void   log_gen0   (Task* task, char* caller) {
+    // ======== 
     //
-    char filename[ 256 ];
+    char filename[ 1024 ];
+    FILE* fd = open_heapdump_logfile( filename, 1024, "gen0" );
 
-    log_if("");
-    log_if("log_gen0: generation-0 heapbuffer dump for pthread->id x=%x, called by %s",  (unsigned int)(task->pthread->tid), caller);
-    log_if("log_gen0:           agegroup0_buffer x=%x",  task->heap->agegroup0_buffer);
-    log_if("log_gen0:  agegroup0_buffer_bytesize x=%x",  task->heap->agegroup0_buffer_bytesize);
-    log_if("log_gen0:           sum of above two x=%x",  (char*)(task->heap->agegroup0_buffer) + task->heap->agegroup0_buffer_bytesize);
-    log_if("log_gen0:    heap_allocation_pointer x=%x",  task->heap_allocation_pointer);
-    log_if("log_gen0:      heap_allocation_limit x=%x",  task->heap_allocation_limit);
-    log_if("log_gen0: real_heap_allocation_limit x=%x",  task->real_heap_allocation_limit);
-    log_if("log_gen0: gen0 buffer contents");
+    log_if("log_gen0: Starting dump to '%s'", filename);
+
+    fprintf(fd," generation-0 heapbuffer dump for pthread->id x=%x, called by %s\n",  (unsigned int)(task->pthread->tid), caller);
+    fprintf(fd,"           agegroup0_buffer p=%p\n",  task->heap->agegroup0_buffer);
+    fprintf(fd,"  agegroup0_buffer_bytesize x=%x\n",  (unsigned int)task->heap->agegroup0_buffer_bytesize);				// Punt
+    fprintf(fd,"           sum of above two p=%p\n",  (char*)(task->heap->agegroup0_buffer) + task->heap->agegroup0_buffer_bytesize);
+    fprintf(fd,"    heap_allocation_pointer p=%p\n",  task->heap_allocation_pointer);
+    fprintf(fd,"      heap_allocation_limit p=%p\n",  task->heap_allocation_limit);
+    fprintf(fd," real_heap_allocation_limit p=%p\n",  task->real_heap_allocation_limit);
+    fprintf(fd," gen0 buffer contents:\n\n");
 
     {   
 	//
@@ -179,8 +244,8 @@ void log_gen0( Task* task, char* caller ) {
 	    if (!IS_TAGWORD(*p))	{
 		//
 		char buf[ 132 ];
-		char* as_ascii = val_sized_unt_as_ascii(buf,*p);
-		log_if("log_gen0: %08x: %08x  %s",  p, *p, as_ascii);
+		char* as_ascii = val_sized_unt_as_ascii(buf,(Val_Sized_Unt)(*p));
+		fprintf(fd," %8p: %08x  %s\n",  p, v2u(*p), as_ascii);
 	    } else {
 		int   words = GET_LENGTH_IN_WORDS_FROM_TAGWORD(*p);
 		char* tag;
@@ -224,10 +289,13 @@ void log_gen0( Task* task, char* caller ) {
 		default:						tag = "???";			break;
 		}
 
-		log_if("log_gen0: %08x: %08x %3d word %s",  p, *p, words, tag);
+		fprintf(fd," %8p: %08x %3d word %s\n",  p, v2u(*p), words, tag);
 	    }
 	}
     }
+
+    close_heapdump_logfile( fd, filename );
+    log_if("log_gen0: Dump to '%s' now complete.", filename);
 }
 
 // COPYRIGHT (c) 1993 by AT&T Bell Laboratories.
--- x
+++ x
@@ -2,6 +2,8 @@
 
 file::set_logger_to (file::LOG_TO_FILE "x.log");
 heap_debug::log_gen0 "x";
+heap_debug::log_gen0 "x";
+
 
 
 
