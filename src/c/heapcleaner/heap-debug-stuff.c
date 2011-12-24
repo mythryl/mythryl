a30e6548de90a12657560ae2d4833b697b5cdffc
Cynbe <cynbe@mythryl.org>
2011-12-24 02:42:03 -0800

    2011-12-24-02.41.47: src/c/heapcleaner/heap-debug-stuff.c: More output tweaking for improved readability.

--- src/c/heapcleaner/heap-debug-stuff.c
+++ src/c/heapcleaner/heap-debug-stuff.c
@@ -227,6 +227,99 @@ void   dump_task   (Task* task, char* caller) {
     log_if("dump_task: Dump to '%s' now complete.", filename);
 }
 
+
+static void   dump_records   (FILE* fd, Val* start, Val* stop) {
+    //        ============
+    //    
+    int words_left_in_record = 0;
+    //
+    for (Val* p = start;
+	      p < stop;
+	      p++
+    ){
+	//
+	if (words_left_in_record > 0
+	||  !IS_TAGWORD(*p)
+	){
+	    if (!words_left_in_record)   fprintf(fd,"\n");
+	    //
+	    char buf[ 132 ];
+	    char* as_ascii = val_sized_unt_as_ascii(buf,(Val_Sized_Unt)(*p));
+	    fprintf(fd," %8p: %08x  %s",  p, v2u(*p), as_ascii);
+	    //
+	    if (!words_left_in_record
+	    && (!(((int)p)&4))					// We use padwords to 64-bit align a record payload, which non-64-bits-aligns the record tagword itself -- so the padword itself is 64-bit aligned.  Unintuitive.
+	    && IS_TAGWORD(p[1])
+	    && GET_BTAG_FROM_TAGWORD(p[1])==EIGHT_BYTE_ALIGNED_NONPOINTER_DATA_BTAG
+	    ){
+		fprintf(fd,"    (padword to 64-bit align payload of following record)");
+	    }
+	    //
+	    fprintf(fd,"\n");
+	    --words_left_in_record;
+
+	} else {
+
+	    int   words = GET_LENGTH_IN_WORDS_FROM_TAGWORD(*p);
+	    Bool  words_is_bogus = FALSE;
+	    Bool    tag_is_bogus = FALSE;
+	    char* tag;
+
+	    switch (GET_BTAG_FROM_TAGWORD(*p)) {
+	    //
+	    case PAIRS_AND_RECORDS_BTAG:				tag = "RECORD";					break;
+	    //
+	    case RW_VECTOR_HEADER_BTAG:
+		words_is_bogus = TRUE;				// 'words' field is abused for additional tag info in this case -- length is implicitly fixed at 2 words.
+		switch (words) {
+		case VECTOR_OF_EIGHT_BYTE_FLOATS_CTAG:		tag = "FLOAT64_RW_VECTOR HEADER";		break;
+		case TYPEAGNOSTIC_VECTOR_CTAG:			tag = "TYPEAGNOSTIC_RW_VECTOR HEADER";		break;
+		case VECTOR_OF_ONE_BYTE_UNTS_CTAG:			tag = "UNT8_RW_VECTOR HEADER";			break;
+		default:	tag_is_bogus = TRUE;			tag = "???_RW_VECTOR HEADER";			break;
+		}
+		words = 2;
+		break;
+	    //
+	    case RO_VECTOR_HEADER_BTAG:
+		words_is_bogus = TRUE;				// 'words' field is abused for additional tag info in this case -- length is implicitly fixed at 2 words.
+		switch (words) {
+		case TYPEAGNOSTIC_VECTOR_CTAG:			tag = "TYPEAGNOSTIC_RO_VECTOR HEADER";		break;
+		case VECTOR_OF_ONE_BYTE_UNTS_CTAG:			tag = "STRING / UNT8_RO_VECTOR HEADER";		break;
+		default:	tag_is_bogus = TRUE;			tag = "???_RO_VECTOR HEADER";			break;
+		}
+		words = 2;
+		break;
+	    //
+	    case RW_VECTOR_DATA_BTAG:				tag = "RW_VECTOR_DATA";				break;
+	    case FOUR_BYTE_ALIGNED_NONPOINTER_DATA_BTAG:		tag = "NONPTR_DATA4";				break;
+	    case EIGHT_BYTE_ALIGNED_NONPOINTER_DATA_BTAG:		tag = "NONPTR_DATA8";				break;
+	    case EXTERNAL_REFERENCE_IN_EXPORTED_HEAP_IMAGE_BTAG:	tag = "EXTERNAL_REF";				break;
+	    case FORWARDED_CHUNK_BTAG:				tag = "FORWARDED";				break;
+	    //
+	    case WEAK_POINTER_OR_SUSPENSION_BTAG:
+		words_is_bogus = TRUE;				// 'words' field is abused for additional tag info in this case -- length is implicitly fixed at 2 words.
+		switch (words) {
+		case UNEVALUATED_LAZY_SUSPENSION_CTAG:		tag = "UNEVAL_SUSPENSION";			break;
+		case   EVALUATED_LAZY_SUSPENSION_CTAG:		tag =   "EVAL_SUSPENSION";			break;
+		case                WEAK_POINTER_CTAG:		tag = "WEAK_PTR";				break;
+		case         NULLED_WEAK_POINTER_CTAG:		tag = "NULLED_WEAK_PTR";			break;
+		default:	tag_is_bogus = TRUE;			tag = "??? (bogus WEAK/LAZY)";			break;
+		}	
+		words = 1;
+		break;
+	    //
+	    default:	tag_is_bogus = TRUE;			tag = "???";					break;
+	    }
+
+	    if (!tag_is_bogus)	words_left_in_record = words;		// If the tag is bogus, the length probably is too, so ignore it.
+
+	    if (words_is_bogus)	fprintf(fd,"\n %8p: %08x %s\n",          p, v2u(*p),        tag);
+	    else			fprintf(fd,"\n %8p: %08x %d-word %s\n",  p, v2u(*p), words, tag);
+	}
+    }
+}
+
+
 // Write to logfile contents of the generation0
 // buffer for a given Task.  No attempt is made
 // to distinguish live from dead data.
@@ -250,93 +343,7 @@ void   dump_gen0   (Task* task, char* caller) {
     fprintf(fd," real_heap_allocation_limit p=%p\n",  task->real_heap_allocation_limit);
     fprintf(fd," gen0 buffer contents:\n\n");
 
-    {   int words_left_in_record = 0;
-	//
-	for (Val* p = task->heap->agegroup0_buffer;
-	          p < task->heap_allocation_pointer;
-	          p++
-	){
-	    //
-	    if (words_left_in_record > 0
-            ||  !IS_TAGWORD(*p)
-	    ){
-		if (!words_left_in_record)   fprintf(fd,"\n");
-		//
-		char buf[ 132 ];
-		char* as_ascii = val_sized_unt_as_ascii(buf,(Val_Sized_Unt)(*p));
-		fprintf(fd," %8p: %08x  %s",  p, v2u(*p), as_ascii);
-		//
-		if (!words_left_in_record
-                && (!(((int)p)&4))					// We use padwords to 64-bit align a record payload, which non-64-bits-aligns the record tagword itself -- so the padword itself is 64-bit aligned.  Unintuitive.
-		&& IS_TAGWORD(p[1])
-		&& GET_BTAG_FROM_TAGWORD(p[1])==EIGHT_BYTE_ALIGNED_NONPOINTER_DATA_BTAG
-		){
-		    fprintf(fd,"    (padword to 64-bit align payload of following record)");
-		}
-		//
-		fprintf(fd,"\n");
-		--words_left_in_record;
-
-	    } else {
-
-		int   words = GET_LENGTH_IN_WORDS_FROM_TAGWORD(*p);
-		Bool  words_is_bogus = FALSE;
-		Bool    tag_is_bogus = FALSE;
-		char* tag;
-		
-		switch (GET_BTAG_FROM_TAGWORD(*p)) {
-		//
-		case PAIRS_AND_RECORDS_BTAG:				tag = "RECORD";					break;
-		//
-		case RW_VECTOR_HEADER_BTAG:
-		    words_is_bogus = TRUE;				// 'words' field is abused for additional tag info in this case -- length is implicitly fixed at 2 words.
-		    switch (words) {
-		    case VECTOR_OF_EIGHT_BYTE_FLOATS_CTAG:		tag = "FLOAT64_RW_VECTOR HEADER";		break;
-		    case TYPEAGNOSTIC_VECTOR_CTAG:			tag = "TYPEAGNOSTIC_RW_VECTOR HEADER";		break;
-		    case VECTOR_OF_ONE_BYTE_UNTS_CTAG:			tag = "UNT8_RW_VECTOR HEADER";			break;
-		    default:	tag_is_bogus = TRUE;			tag = "???_RW_VECTOR HEADER";			break;
-		    }
-		    words = 2;
-		    break;
-		//
-		case RO_VECTOR_HEADER_BTAG:
-		    words_is_bogus = TRUE;				// 'words' field is abused for additional tag info in this case -- length is implicitly fixed at 2 words.
-		    switch (words) {
-		    case TYPEAGNOSTIC_VECTOR_CTAG:			tag = "TYPEAGNOSTIC_RO_VECTOR HEADER";		break;
-		    case VECTOR_OF_ONE_BYTE_UNTS_CTAG:			tag = "STRING / UNT8_RO_VECTOR HEADER";		break;
-		    default:	tag_is_bogus = TRUE;			tag = "???_RO_VECTOR HEADER";			break;
-		    }
-		    words = 2;
-		    break;
-		//
-		case RW_VECTOR_DATA_BTAG:				tag = "RW_VECTOR_DATA";				break;
-		case FOUR_BYTE_ALIGNED_NONPOINTER_DATA_BTAG:		tag = "NONPTR_DATA4";				break;
-		case EIGHT_BYTE_ALIGNED_NONPOINTER_DATA_BTAG:		tag = "NONPTR_DATA8";				break;
-		case EXTERNAL_REFERENCE_IN_EXPORTED_HEAP_IMAGE_BTAG:	tag = "EXTERNAL_REF";				break;
-		case FORWARDED_CHUNK_BTAG:				tag = "FORWARDED";				break;
-		//
-		case WEAK_POINTER_OR_SUSPENSION_BTAG:
-		    words_is_bogus = TRUE;				// 'words' field is abused for additional tag info in this case -- length is implicitly fixed at 2 words.
-		    switch (words) {
-		    case UNEVALUATED_LAZY_SUSPENSION_CTAG:		tag = "UNEVAL_SUSPENSION";			break;
-		    case   EVALUATED_LAZY_SUSPENSION_CTAG:		tag =   "EVAL_SUSPENSION";			break;
-		    case                WEAK_POINTER_CTAG:		tag = "WEAK_PTR";				break;
-		    case         NULLED_WEAK_POINTER_CTAG:		tag = "NULLED_WEAK_PTR";			break;
-		    default:	tag_is_bogus = TRUE;			tag = "??? (bogus WEAK/LAZY)";			break;
-		    }	
-		    words = 1;
-		    break;
-		//
-		default:	tag_is_bogus = TRUE;			tag = "???";					break;
-		}
-
-		if (!tag_is_bogus)	words_left_in_record = words;		// If the tag is bogus, the length probably is too, so ignore it.
-
-		if (words_is_bogus)	fprintf(fd,"\n %8p: %08x %s\n",          p, v2u(*p),        tag);
-		else			fprintf(fd,"\n %8p: %08x %d-word %s\n",  p, v2u(*p), words, tag);
-	    }
-	}
-    }
+    dump_records( fd, task->heap->agegroup0_buffer, task->heap_allocation_pointer );
 
     close_heapdump_logfile( fd, filename );
     log_if("dump_gen0: Dump to '%s' now complete.", filename);
@@ -373,12 +380,8 @@ static void   dump_sib   (Task* task, FILE* fd, Sib* sib) {
 
 static void   dump_record_sib   (Task* task, FILE* fd, Sib* sib) {
     //        ===============
-    for (Val* p = sib->tospace;
-	      p < sib->next_tospace_word_to_allocate;
-	      p++
-    ){
-	fprintf(fd," %8p: %08x\n",  p, v2u(*p));
-    }    
+    //
+    dump_records   (fd, sib->tospace, sib->next_tospace_word_to_allocate);
 }
 
 static void   dump_pair_sib   (Task* task, FILE* fd, Sib* sib) {	// The pairs in the pair sib have no tag/length word -- avoiding that overhead is the main point of having a separate sib for pairs.
@@ -395,6 +398,7 @@ static void   dump_pair_sib   (Task* task, FILE* fd, Sib* sib) {	// The pairs in
 
 static void   dump_string_sib   (Task* task, FILE* fd, Sib* sib) {
     //        ===============
+    //
     for (Val* p = sib->tospace;
 	      p < sib->next_tospace_word_to_allocate;
 	      p++
@@ -407,12 +411,15 @@ static void   dump_string_sib   (Task* task, FILE* fd, Sib* sib) {
 
 static void   dump_vector_sib   (Task* task, FILE* fd, Sib* sib) {
     //        ===============
-    for (Val* p = sib->tospace;
-	      p < sib->next_tospace_word_to_allocate;
-	      p++
-    ){
-	fprintf(fd," %8p: %08x\n",  p, v2u(*p));
-    }    
+    //
+    dump_records   (fd, sib->tospace, sib->next_tospace_word_to_allocate);
+
+//    for (Val* p = sib->tospace;
+//	      p < sib->next_tospace_word_to_allocate;
+//	      p++
+//    ){
+//	fprintf(fd," %8p: %08x\n",  p, v2u(*p));
+//    }    
 }
 
 
@@ -431,36 +438,36 @@ void   dump_gens   (Task* task, char* caller) {
 
     log_if("dump_gens: Starting dump to '%s'", filename);
 
-    fprintf(fd,"Generation-1 dump generated by dump_gens in src/c/heapcleaner/heap-debug-stuff.c\n"	);
-    fprintf(fd,"\n"											);
+    fprintf(fd,"Generation-1 dump generated by dump_gens in src/c/heapcleaner/heap-debug-stuff.c\n"							);
+    fprintf(fd,"\n"																	);
 
     for (int a = 0;
              a < task->heap->active_agegroups;
              a++
     ){
-	fprintf(fd,"\n"											);
-	fprintf(fd,"\n"											);
-	fprintf(fd,"=====================\n"											);
+	fprintf(fd,"\n"																	);
+	fprintf(fd,"\n"																	);
+	fprintf(fd,"=====================\n"														);
 	fprintf(fd,"Dump of generation %d\n", a+1);
-	fprintf(fd,"\n"											);
+	fprintf(fd,"\n"																	);
 
 	Agegroup* ag  =  heap->agegroup[a];		// Get pointer to our agegroup.
 
-	fprintf(fd,"                     task p= %p\n",					task		);
-	fprintf(fd,"                     heap p= %p  (task->heap)\n",			heap		);
-	fprintf(fd,"                      ag p= %p  (agegroup%d)\n",a+1,		ag		);
-	fprintf(fd,"\n"											);
-	fprintf(fd,"                 ag->age d= %d\n",					ag->age	);
-	fprintf(fd,"                 ag->cleanings d= %d\n",				ag->cleanings	);
-	fprintf(fd,"                 ag->ratio     d= %d\n",				ag->ratio	);
-	fprintf(fd,"                 ag->last_cleaning_count_of_younger_agegroup d= %d\n",	ag->last_cleaning_count_of_younger_agegroup	);
-	fprintf(fd,"                 ag->tospace_ram_region p= %p\n",			ag->tospace_ram_region				);
-	fprintf(fd,"                 ag->fromspace_ram_region p= %p\n",			ag->fromspace_ram_region			);
-	fprintf(fd,"                 ag->saved_fromspace_ram_region p= %p\n",		ag->saved_fromspace_ram_region			);
-	fprintf(fd,"                 ag->coarse_inter_agegroup_pointers_map p= %p\n",	ag->coarse_inter_agegroup_pointers_map		);
+	fprintf(fd,"                     task p= %p\n",					task								);
+	fprintf(fd,"                     heap p= %p  (task->heap)\n",			heap								);
+	fprintf(fd,"                      ag p= %p  (agegroup%d)\n", a+1,		ag								);
+	fprintf(fd,"\n"																	);
+	fprintf(fd,"                 ag->age d= %d\n",						ag->age							);
+	fprintf(fd,"                 ag->cleanings d= %d\n",					ag->cleanings						);
+	fprintf(fd,"                 ag->ratio     d= %d\n",					ag->ratio						);
+	fprintf(fd,"                 ag->last_cleaning_count_of_younger_agegroup d= %d\n",	ag->last_cleaning_count_of_younger_agegroup		);
+	fprintf(fd,"                 ag->tospace_ram_region p= %p\n",				ag->tospace_ram_region					);
+	fprintf(fd,"                 ag->fromspace_ram_region p= %p\n",				ag->fromspace_ram_region				);
+	fprintf(fd,"                 ag->saved_fromspace_ram_region p= %p\n",			ag->saved_fromspace_ram_region				);
+	fprintf(fd,"                 ag->coarse_inter_agegroup_pointers_map p= %p\n",		ag->coarse_inter_agegroup_pointers_map			);
 
 
-	fprintf(fd,"                 ag->hugechunks[0] p= %p   (CODE__HUGE_ILK)\n",		ag->hugechunks[0]				);
+	fprintf(fd,"                 ag->hugechunks[0] p= %p   (CODE__HUGE_ILK)\n",		ag->hugechunks[0]					);
 
 	fprintf(fd,"                 ag->sib[%d] p= %p    (RECORD_ILK)\n",			RECORD_ILK,	ag->sib[RECORD_ILK]			);
 	fprintf(fd,"                 ag->sib[%d] p= %p    (  PAIR_ILK)\n",			  PAIR_ILK,	ag->sib[  PAIR_ILK]			);
@@ -468,22 +475,21 @@ void   dump_gens   (Task* task, char* caller) {
 	fprintf(fd,"                 ag->sib[%d] p= %p    (VECTOR_ILK)\n",			VECTOR_ILK,	ag->sib[VECTOR_ILK]			);
 
 
-	fprintf(fd,"\nRECORD_ILK sib details:\n");    dump_sib(task,fd, ag->sib[RECORD_ILK] );
-	fprintf(fd,"\n  PAIR_ILK sib details:\n");    dump_sib(task,fd, ag->sib[  PAIR_ILK] );
-	fprintf(fd,"\nSTRING_ILK sib details:\n");    dump_sib(task,fd, ag->sib[STRING_ILK] );
-	fprintf(fd,"\nVECTOR_ILK sib details:\n");    dump_sib(task,fd, ag->sib[VECTOR_ILK] );
+	fprintf(fd,"\n------------------------\nRECORD_ILK sib details:\n");    dump_sib(task,fd, ag->sib[RECORD_ILK] );
+	fprintf(fd,"\n------------------------\n  PAIR_ILK sib details:\n");    dump_sib(task,fd, ag->sib[  PAIR_ILK] );
+	fprintf(fd,"\n------------------------\nSTRING_ILK sib details:\n");    dump_sib(task,fd, ag->sib[STRING_ILK] );
+	fprintf(fd,"\n------------------------\nVECTOR_ILK sib details:\n");    dump_sib(task,fd, ag->sib[VECTOR_ILK] );
 
 
-	fprintf(fd,"\nRECORD_ILK sib contents:\n");	  dump_record_sib( task, fd, ag->sib[RECORD_ILK] );
-	fprintf(fd,"\n  PAIR_ILK sib contents:\n");	    dump_pair_sib( task, fd, ag->sib[  PAIR_ILK] );
-	fprintf(fd,"\nSTRING_ILK sib contents:\n");	  dump_string_sib( task, fd, ag->sib[STRING_ILK] );
-	fprintf(fd,"\nVECTOR_ILK sib contents:\n");	  dump_vector_sib( task, fd, ag->sib[VECTOR_ILK] );
+	fprintf(fd,"\n------------------------\nRECORD_ILK sib contents -- immutable pointerblocks:\n");		dump_record_sib( task, fd, ag->sib[RECORD_ILK] );
+	fprintf(fd,"\n------------------------\n  PAIR_ILK sib contents -- immutable pointerpairs:\n");			  dump_pair_sib( task, fd, ag->sib[  PAIR_ILK] );
+	fprintf(fd,"\n------------------------\nSTRING_ILK sib contents -- immutable nonpointer stuff:\n");		dump_string_sib( task, fd, ag->sib[STRING_ILK] );
+	fprintf(fd,"\n------------------------\nVECTOR_ILK sib contents -- all mutable data goes in this sib:\n");	dump_vector_sib( task, fd, ag->sib[VECTOR_ILK] );
     }
     close_heapdump_logfile( fd, filename );
     log_if("dump_gens: Dump to '%s' now complete.", filename);
 }
 
-// COPYRIGHT (c) 1993 by AT&T Bell Laboratories.
-// Subsequent changes by Jeff Prothero Copyright (c) 2010-2011,
+// Jeff Prothero Copyright (c) 2010-2011,
 // released under Gnu Public Licence version 3.
 
